dce07df431e3c1475be9151440ebad371bc14186
Marco Chiappetta <mardurhack@gmail.com>
2012-06-13 00:53:20 +0200

    - main.c: Dummy initialization added - doc: umps2 structure added

--- src/main.c
+++ src/main.c
@@ -11,19 +11,51 @@
 #define MIN_PCB_PRIORITY		0
 #define DEFAULT_PCB_PRIORITY		5
 
+#define NUM_CPU	4
 
 /******************* MAIN TEST **********************/
 
 int main(void)
 {
-	/* DEBUGGING TEST */
-	int ciao = 23+5; 
-	debug(38, ciao); /* expected 28 in $a1 */
-	debug(39, TRUE); /* expected 1 in $a1 */
-	debug(40, FALSE); /* expected 0 in $a1 */
+	/* Popolare le 4 "New Areas" nel "ROM Reserved Frame" */
+	/* Popolare le 4 "New Areas" nel "ROM Reserved Frame" */
+	/* Ogni CPU può essere rappresentata tramite una struttura dati
+	 * all'interno della quale memorizziamo l'ID della CPU e le varie
+	 * aree Old/New */
+	typedef struct Areas{
+		state_t Sysbp;
+		state_t Trap;
+		state_t Tlb;
+		state_t Ints;
+	} Areas;
+	 
+	typedef struct CPU{
+		int id;
+		Areas new;
+		Areas old;
+	} CPU;
+	 
+	CPU procs[NUM_CPU];
+	Areas dummy;
+	initState_t(&(dummy.Sysbp));
+	initState_t(&(dummy.Trap));
+	initState_t(&(dummy.Tlb));
+	initState_t(&(dummy.Ints));
+	
+	/* New Area iniziali (comuni a tutte le CPU) */
+	int id;
+	for (id = 1; id < NUM_CPU; id++){
+		CPU *cur = &(procs[id]);
+		cur->id = id;
+		debug(51, id);
+		INITCPU(id, &(dummy), &(cur->new.Sysbp));
+		INITCPU(id, &(dummy), &(cur->new.Trap));
+		INITCPU(id, &(dummy), &(cur->new.Tlb));
+		INITCPU(id, &(dummy), &(cur->new.Ints));
+	}
 	
 	
-    /* Popolare le 4 "New Areas" nel "ROM Reserved Frame" */
+    
 	
     /* Inizializzare le strutture dati di fase 1 */
     initPcbs();
